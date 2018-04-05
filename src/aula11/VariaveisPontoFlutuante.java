4626239bbe41ffa94bb4da739b8d8397de68f461
fabiodevs <37477500+fabiodevs@users.noreply.github.com>
2018-04-05 11:31:47 -0300

    Aula 11
    Variáveis Ponto Flutuante

---
+++ src/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,14 @@
+package aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+		double valorPassagem = 2.90;
+		float valorTomate = 3.95f;
+		
+		System.out.println("valor da passagem = R$" + valorPassagem);
+		System.out.println("Valor do tomate = R$"+valorTomate);
+
+	}
+
+}
