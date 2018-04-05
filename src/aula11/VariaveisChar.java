7f0b092bc3d8e3547cf4b69107080e3f9b354dca
fabiodevs <37477500+fabiodevs@users.noreply.github.com>
2018-04-05 11:48:18 -0300

    Aula 11
    Variavel Char

---
+++ src/aula11/VariaveisChar.java
@@ -0,0 +1,15 @@
+package aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		char o = 'o';
+		char i = 'i';
+		
+		char codigo = 0x00E1;
+		
+		System.out.println(""+o+i + codigo);
+
+	}
+
+}
